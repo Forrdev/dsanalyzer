652acc68264faa47d593aaff3edd02f3e966bded
sappyoak <81883643+sappyoak@users.noreply.github.com>
2026-08-13 15:10:57 -0400

    Includes the shared and domain modules in the main app project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":shared"))
+    implementation(project(":domain"))
+
     implementation(compose.desktop.currentOs)
     implementation(compose.material3)
     implementation(compose.materialIconsExtended)
